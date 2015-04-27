d9c0d09556332a6a9f78bdb8b45bb7946bdeef80
yangbo <yangbocp@feinno.com>
2015-04-27 09:54:30 +0800

    delete

--- app/src/main/java/io/rong/app/fragment/DeSearchbarFragment.java
+++ app/src/main/java/io/rong/app/fragment/DeSearchbarFragment.java
@@ -3,7 +3,6 @@ package io.rong.app.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +12,11 @@ import io.rong.app.R;
 /**
  * Created by rongdev on 15/4/10.
  */
-public class DeSearchbarFragment extends Fragment{
+public class DeSearchbarFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.searchbar, null);
-
-
 
         return view;
     }
--- app/src/main/java/io/rong/app/fragment/TestFragment.java
+++
@@ -1,7 +0,0 @@
-package io.rong.app.fragment;
-
-/**
- * Created by Administrator on 2015/1/17.
- */
-public class TestFragment {
-}
